3c7259587fe2dfc9b59f6c7a715c23420ab8eb3e
Jencir Lee <jli05@icloud.com>
2017-04-29 14:41:53 +0100

    change to use left singular vectors for ALS initialisation

--- src/main/scala/edu/uci/eecs/spectralLDA/algorithm/ALS.scala
+++ src/main/scala/edu/uci/eecs/spectralLDA/algorithm/ALS.scala
@@ -7,38 +7,27 @@ package edu.uci.eecs.spectralLDA.algorithm
 */
 
 import edu.uci.eecs.spectralLDA.utils.{AlgebraUtil, TensorOps}
-import breeze.linalg.{*, DenseMatrix, DenseVector, diag, inv, max, min, norm, qr}
+import breeze.linalg.{*, DenseMatrix, DenseVector, diag, max, min, norm, svd}
 import breeze.stats.distributions.{Gaussian, Rand, RandBasis}
 
-/** Tensor decomposition by Alternating Least Square (ALS)
+/** CANDECOMP/PARAFAC decomposition via Alternating Least Square (ALS)
   *
-  * Suppose dimK-by-dimK-by-dimK symmetric tensor T can be decomposed as sum of rank-1 tensors
-  *
-  * $$ T = \sum_{i=1}^{dimK} \lambda_i a_i\otimes b_i\otimes c_i $$
-  *
-  * If we pool all \lambda_i in the vector \Lambda, all the column vectors \lambda_i a_i in A,
-  * b_i in B, c_i in C, then
-  *
-  * $$ T^{1} = A \diag(\Lambda)(C \khatri-rao product B)^{\top} $$
-  *
-  * where T^{1} is a dimK-by-(dimK^2) matrix for the unfolded T.
   *
   * @param dimK               tensor T is of shape dimK-by-dimK-by-dimK
-  * @param thirdOrderMoments  dimK-by-(dimK*dimK) matrix for the unfolded 3rd-order moments
-  *                           $\sum_{i=1}^k\alpha_i\beta_i^{\otimes 3}$
+  * @param tensor3D           dimK-by-(dimK*dimK) matrix for the unfolded M3
   * @param maxIterations      max iterations for the ALS algorithm
   * @param tol                tolerance. the dot product threshold is 1-tol
   * @param restarts           number of restarts of the ALS loop
   */
 class ALS(dimK: Int,
-          thirdOrderMoments: DenseMatrix[Double],
+          tensor3D: DenseMatrix[Double],
           maxIterations: Int = 500,
           tol: Double = 1e-6,
           restarts: Int = 5)
   extends Serializable {
   assert(dimK > 0, "The number of topics dimK must be positive.")
-  assert(thirdOrderMoments.rows == dimK && thirdOrderMoments.cols == dimK * dimK,
-    "The thirdOrderMoments must be dimK-by-(dimK * dimK) unfolded matrix")
+  assert(tensor3D.rows == dimK && tensor3D.cols == dimK * dimK,
+    "The tensor3D must be dimK-by-(dimK * dimK) unfolded matrix")
 
   assert(maxIterations > 0, "Max iterations must be positive.")
   assert(tol > 0.0, "tol must be positive and probably close to 0.")
@@ -65,14 +54,11 @@ class ALS(dimK: Int,
     var reconstructedLoss: Double = 0.0
     var optimalReconstructedLoss: Double = Double.PositiveInfinity
 
+    val svd.SVD(a0, _, _) = svd(tensor3D)
     for (s <- 0 until restarts) {
-      val qr.QR(a0, _) = qr(DenseMatrix.rand[Double](dimK, dimK, gaussian))
-      val qr.QR(b0, _) = qr(DenseMatrix.rand[Double](dimK, dimK, gaussian))
-      val qr.QR(c0, _) = qr(DenseMatrix.rand[Double](dimK, dimK, gaussian))
-
-      var A = a0
-      var B = b0
-      var C = c0
+      var A = a0.copy
+      var B = a0.copy
+      var C = a0.copy
 
       var A_prev = DenseMatrix.zeros[Double](dimK, dimK)
       var lambda: breeze.linalg.DenseVector[Double] = DenseVector.zeros[Double](dimK)
@@ -83,15 +69,15 @@ class ALS(dimK: Int,
         !AlgebraUtil.isConverged(A_prev, A, dotProductThreshold = 1 - tol))) {
         A_prev = A.copy
 
-        val (updatedA, updatedLambda1) = updateALSIteration(thirdOrderMoments, B, C)
+        val (updatedA, updatedLambda1) = updateALSIteration(tensor3D, B, C)
         A = updatedA
         lambda = updatedLambda1
 
-        val (updatedB, updatedLambda2) = updateALSIteration(thirdOrderMoments, C, A)
+        val (updatedB, updatedLambda2) = updateALSIteration(tensor3D, C, A)
         B = updatedB
         lambda = updatedLambda2
 
-        val (updatedC, updatedLambda3) = updateALSIteration(thirdOrderMoments, A, B)
+        val (updatedC, updatedLambda3) = updateALSIteration(tensor3D, A, B)
         C = updatedC
         lambda = updatedLambda3
 
@@ -101,7 +87,7 @@ class ALS(dimK: Int,
       }
       println("Finished ALS iterations.")
 
-      reconstructedLoss = TensorOps.dmatrixNorm(thirdOrderMoments - A * diag(lambda) * TensorOps.krprod(C, B).t)
+      reconstructedLoss = TensorOps.dmatrixNorm(tensor3D - A * diag(lambda) * TensorOps.krprod(C, B).t)
       println(s"Reconstructed loss: $reconstructedLoss\tOptimal reconstructed loss: $optimalReconstructedLoss")
 
       if (reconstructedLoss < optimalReconstructedLoss) {
